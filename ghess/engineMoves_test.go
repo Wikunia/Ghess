a3b5ffc54a7397afc0038034833f889d8151bb67
Ole Kroeger <o.kroeger@opensourc.es>
2021-04-17 15:31:23 +0200

    fix again...

--- ghess/engineMoves_test.go
+++ ghess/engineMoves_test.go
@@ -17,7 +17,7 @@ var engineMovesTests = []engineMoves{
 	{"8/8/B3k3/8/4K3/6r1/8/5N2 w - - 0 1", "checkCaptureRandom", []string{"a6c8", "a6c4"}},
 	{"8/8/B3k3/8/4K3/6r1/8/5N2 w - - 0 1", "checkCaptureRandom", []string{"a6c8", "a6c4"}},
 	{"5k2/1P6/2P2K2/8/8/8/8/8 w - - 0 1", "random", []string{"b7b8q", "b7b8r", "b7b8b", "b7b8n", "c6c7", "f6e6", "f6e5", "f6f5", "f6g5", "f6g6"}},
-	{"5k2/1P6/2P2K2/8/8/8/8/8 w - - 0 1", "captureRandom", []string{"b7b8q", "b7b8r", "b7b8b", "b7b8n", "c6c7", "f6e6", "f6e5", "f6ef5", "f6g5", "f6g6"}},
+	{"5k2/1P6/2P2K2/8/8/8/8/8 w - - 0 1", "captureRandom", []string{"b7b8q", "b7b8r", "b7b8b", "b7b8n", "c6c7", "f6e6", "f6e5", "f6f5", "f6g5", "f6g6"}},
 	{"5k2/1P6/2P2K2/8/8/8/8/8 w - - 0 1", "checkCaptureRandom", []string{"b7b8q", "b7b8r"}},
 	{"5k2/1P6/2P2K2/8/8/8/8/8 w - - 0 1", "alphaBeta", []string{"b7b8q", "b7b8r"}},
 }
